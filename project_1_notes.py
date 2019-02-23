7e70959f5c72e5c128ea644303bc16b2d8b86d6e
phoshell <charlotte.avocado@gmail.com>
2019-02-23 11:44:30 -0800

    repo day3

---
+++ project_1_notes.py
@@ -0,0 +1,32 @@
+# Class Project API key
+# AIzaSyAHf3xaYzApF-x8q1CM7VneuZmCchRiIwg
+
+# Google code for reddit/memes 
+
+gkey = "AIzaSyDloEPbhhkbSdjex_QLpQotMOp5HrQBEkI"
+
+# # Search engine ID
+cx = "000783178101923127511:vw462omy5uo"
+
+# <script>
+#   (function() {
+#     var cx = '000783178101923127511:vw462omy5uo';
+#     var gcse = document.createElement('script');
+#     gcse.type = 'text/javascript';
+#     gcse.async = true;
+#     gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+#     var s = document.getElementsByTagName('script')[0];
+#     s.parentNode.insertBefore(gcse, s);
+#   })();
+# </script>
+# <gcse:search></gcse:search>
+
+
+
+# '''
+
+# request.get(url)
+# url = "https://www.googleapis.com/customsearch/v1?q=kermit&cx=000783178101923127511%3Avw462omy5uo&exactTerms=kermit&key={YOUR_API_KEY}
+
+# '''
+
